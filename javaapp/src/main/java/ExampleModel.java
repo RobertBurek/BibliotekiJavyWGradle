c80e54b3621bea88083329e23e00e2cf94851797
Robert Burek <robertburek@wp.pl>
2019-07-05 12:29:39 +0200

    Czyszczenie - przygotowanie pod kolejną bibliotekę

--- javaapp/src/main/java/ExampleModel.java
+++ javaapp/src/main/java/ExampleModel.java
@@ -12,69 +12,11 @@ import lombok.extern.java.Log;
 @Data
 public class ExampleModel {
 
-    //@EqualsAndHashCode.Include  - ten wskazany
     private String hairColor;
     private int heigth;
     @EqualsAndHashCode.Exclude
-    private int width;  //wykluczony z Equals
+    private int width;
+    @EqualsAndHashCode.Exclude
     private boolean isMale;
     private int age;
-
-    //    Builder  wtedy można zapisać instancje w ten
-//    sposób: exampleModelDuplicate.setHeigth(178).setHairColor("Blond");
-    public ExampleModel myHeigth(int heigth) {
-        this.heigth = heigth;
-        return this;
-    }
-
-    public ExampleModel myHairColor(String hairColor) {
-        this.hairColor = hairColor;
-        return this;
-    }
-
-    public ExampleModel myWidth(int width) {
-        this.width = width;
-        log.info("Przypisałem wartość WIDTH");
-        return this;
-    }
-
-    public ExampleModel myMale(boolean male) {
-        isMale = male;
-        return this;
-    }
-
-    public ExampleModel myAge(int age) {
-        this.age = age;
-        return this;
-    }
-    //    Konstruktory wygenerowane poleceniem Intellij
-//    public ExampleModel(String hairColor, int heigth, boolean isMale, int age) {
-//        this.hairColor = hairColor;
-//        this.heigth = heigth;
-//        this.isMale = isMale;
-//        this.age = age;
-//    }
-//
-//    public ExampleModel() {
-//    }
-
-
-    //    Metody wygenerowane poleceniem Intellij
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        ExampleModel that = (ExampleModel) o;
-//        return heigth == that.heigth &&
-//                width == that.width &&
-//                isMale == that.isMale &&
-//                age == that.age &&
-//                Objects.equals(hairColor, that.hairColor);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(hairColor, heigth, width, isMale, age);
-//    }
 }
--- javaapp/src/main/java/Main.java
+++ javaapp/src/main/java/Main.java
@@ -1,71 +1,32 @@
-import lombok.AllArgsConstructor;
 import lombok.extern.java.Log;
-
-import java.util.logging.Logger;
 
 /**
  * Created by Robert Burek
  */
 
 @Log
-@AllArgsConstructor  //adnotacja z lomboka
 public class Main {
-
-    Boolean a;
-    Boolean b;
-    Boolean c;
-
-    //logger ręcznie napisany
-    private static final Logger myLog = java
-            .util
-            .logging
-            .Logger
-            .getLogger(Main.class.getName());
 
     public static void main(String[] args) {
 
-        new Main(true, true, true);
         ExampleModel exampleModel = new ExampleModel("Blond", 178, 45, true, 40);
+        System.out.print("exampleModel: ");
         System.out.println(exampleModel);
 
-//        Ręcznie napisane metody zwracajace klasę z ustawionym polem
-        ExampleModel exampleModelBuilder = new ExampleModel("", 0, 0, true, 0);
-        exampleModelBuilder
-                .myAge(54)
-                .myHairColor("Red")
-                .myMale(true)
-                .myWidth(45)
-                .myHeigth(178);
-        System.out.print("exampleModelBuilder: ");
-        System.out.println(exampleModelBuilder);
-
-//        Metody wygenerowane przez LOMBOKA
-        ExampleModel exampleModelBuilder2 = ExampleModel.builder()
-                .age(54)
-                .isMale(true)
-                .hairColor("Red")
+        ExampleModel exampleModelDuplicate = ExampleModel.builder()
+                .age(40)
+                .isMale(false)
+                .hairColor("Blond")
                 .heigth(178)
                 .width(60)
                 .build();
-        System.out.print("exampleModelBuilder2: ");
-        System.out.println(exampleModelBuilder2);
-        System.out.println("exampleModelBuilder  vs  exampleModelBuilder2");
-        System.out.println(exampleModelBuilder.equals(exampleModelBuilder2));
-        if (exampleModelBuilder.equals(exampleModelBuilder2))
-            log.info("Obiekty są takie same! - info LOMBOK");
-
-        ExampleModel exampleModelDuplicate = new ExampleModel("", 0, 0, true, 0);
-        exampleModelDuplicate.setAge(40);
-        exampleModelDuplicate.setHeigth(200);
-        exampleModelDuplicate.setHairColor("Blond");
-        exampleModelDuplicate.setWidth(20);
-        exampleModelDuplicate.setMale(true);
         System.out.print("exampleModelDuplicate: ");
         System.out.println(exampleModelDuplicate);
-
+        System.out.println("");
         System.out.println("exampleModel  vs  exampleModelDuplicate");
         System.out.println(exampleModel.equals(exampleModelDuplicate));
-        if (!exampleModel.equals(exampleModelDuplicate))
-                myLog.info("Obiekty nie są takie same! - myInfo");
+        if (exampleModel.equals(exampleModelDuplicate))
+            log.info("Obiekty są takie same!");
+
     }
 }
