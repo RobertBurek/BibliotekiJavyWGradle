a1d0d26bb1398cb049b4d19d826fa1ab2fe17a0b
Robert Burek <robertburek@wp.pl>
2019-07-05 13:08:55 +0200

    Gson  -  toJson() oraz fromJson()

--- javaapp/src/main/java/Main.java
+++ javaapp/src/main/java/Main.java
@@ -8,6 +8,8 @@ import lombok.extern.java.Log;
 @Log
 public class Main {
 
+//    public static final String json ="{\"hairColor\":\"Blond\",\"heigth\":178,\"width\":45,\"isMale\":true,\"age\":40}";
+
     public static void main(String[] args) {
 
         ExampleModel exampleModel = new ExampleModel("Blond", 178, 45, true, 40);
@@ -27,6 +29,19 @@ public class Main {
         System.out.println("exampleModel  vs  exampleModelDuplicate");
         System.out.println(exampleModel.equals(exampleModelDuplicate));
         if (exampleModel.equals(exampleModelDuplicate))
+            log.info("Obiekty są takie same!");
+
+        System.out.println("-----------------------Gson------------------------");
+        String json = new Gson().toJson(exampleModel);
+        // Załóżmy, że string json przyszedł z internetu lub z pliku
+        ExampleModel fromJsonExampleModel = new Gson().fromJson(json,ExampleModel.class);
+
+        System.out.print("fromJsonExampleModel: ");
+        System.out.println(fromJsonExampleModel);
+        System.out.println("");
+        System.out.print("exampleModel  vs  fromJsonExampleModel : ");
+        System.out.println(exampleModel.equals(fromJsonExampleModel));
+        if (exampleModel.equals(fromJsonExampleModel))
             log.info("Obiekty są takie same!");
 
         new Gson();
