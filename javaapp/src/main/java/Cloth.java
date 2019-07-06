110a219f551890086df254b1a63f498359066823
Robert Burek <robertburek@wp.pl>
2019-07-07 00:38:27 +0200

    OKHttp - pobieranie danych w formacie Json z API
       generator Json : https://www.json-generator.com/

--- javaapp/src/main/java/Cloth.java
+++ javaapp/src/main/java/Cloth.java
@@ -1,21 +1,18 @@
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 /**
  * Created by Robert Burek
  */
-
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
-@EqualsAndHashCode    // albo tutaj adnotacja albo w klasie ExampleModel wykluczenie z equals
+@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@ToString
+//@EqualsAndHashCode    // albo tutaj adnotacja albo w klasie ExampleModel wykluczenie z equals
 public class Cloth {
     //    @Getter       wykorzystujemy tylko konstruktora, więc nie jest potrzebne, wypada aby był dostęp do pól w klasie
 //    @Setter       wykorzystujemy tylko konstruktora, więc nie jest potrzebne, wypada aby był dostęp do pól w klasie
     private String color;
     //    @Getter       wykorzystujemy tylko konstruktora, więc nie jest potrzebne, wypada aby był dostęp do pól w klasie
 //    @Setter       wykorzystujemy tylko konstruktora, więc nie jest potrzebne, wypada aby był dostęp do pól w klasie
-    private boolean isNew;
+    private Boolean isNew;
 }
--- javaapp/src/main/java/Main.java
+++ javaapp/src/main/java/Main.java
@@ -1,4 +1,12 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import lombok.extern.java.Log;
+import okhttp3.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
 
 /**
  * Created by Robert Burek
@@ -8,6 +16,35 @@ import lombok.extern.java.Log;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+
+        //Tworzymy klienta http
+        OkHttpClient client = new OkHttpClient();
+
+        //Tworzemy zapytanie http (gdzie są dane?)
+        // generator Json : https://www.json-generator.com/
+        Request request = new Request.Builder()
+                .url("http://www.json-generator.com/api/json/get/cfcBGBkbDm?indent=2")
+                .build();
+
+        //Tworzymy reakcję na pobranie lub nie pobranie danych
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                log.info(e.toString());
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                String json = response.body().string();
+                Type type = new TypeToken<List<Cloth>>() {
+                }.getType();
+                List<Cloth> cloths = new Gson().fromJson(json, type);
+                log.info(cloths.get(0).toString());
+                log.info(cloths.toString());
+            }
+        });
+
+        log.info("koniec bloku");
 
     }
 }
