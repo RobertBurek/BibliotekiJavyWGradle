855f90f4e8d8189c18de2edde9057276b53fb9ee
Robert Burek <robertburek@wp.pl>
2019-07-08 00:00:35 +0200

    GUICE - zasięg

--- javaapp/src/main/java/injection/injectmodule/RepositoryModule.java
+++ javaapp/src/main/java/injection/injectmodule/RepositoryModule.java
@@ -2,6 +2,8 @@ package injection.injectmodule;
 
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
 import com.google.inject.name.Names;
 import injection.Repository;
 
@@ -14,7 +16,12 @@ public class RepositoryModule extends AbstractModule {
     protected void configure() {
         bind(String.class).annotatedWith(Names.named("DB_URL")).toInstance("localhost");
         bind(Repository.class).toProvider(DatabaseProvider.class);
+        //1. metoda zasięgu dla Database
+//        bind(Repository.class).toProvider(DatabaseProvider.class).in(Singleton.class);
     }
 
+        //2. medota zasięgu dla Database
+//    @Provides @Singleton
+//    public Repository getRepo(){...}
 
 }
